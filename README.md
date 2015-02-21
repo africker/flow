5964ece965a0535dda28e92c116010efa625521a
jeffreywolf <wolfjeff@ucla.edu>
2015-02-21 12:59:55 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Introduction
+------------
+
+Flow is a python pipeline that uses the Windows executables of Terrain Analysis Using Digital Elevation Models (<a href="http://hydrology.usu.edu/taudem/taudem5/index.html">TauDEM</a>) to calculate flow accumulation for the multiscale elevation output of <a href="https://github.com/jeffreywolf/surface">Surface Analysis Tools (e.g. surface.py)</a>. <a href="http://hydrology.usu.edu/taudem/taudem5/index.html">TauDEM</a> is a fast parallel implementation of a large suite of hydrology tools. This wrapper facilitates calculation of Flow Accumulation using either the D8 or the D-infinity algorithm () at multiple spatial scales. 
+
+Notes
+-----
+<a href="http://hydrology.usu.edu/taudem/taudem5/downloads.html">Download the TauDEM tools for ArcGIS 10</a>.  
+
+All you need to use them outside ArcGIS is the path to the executable folder. For version 5, the subfolder is labelled TauDEM5Exe.
+
+
+References
+----------
+http://hydrology.usu.edu/taudem/taudem5/index.html
+
+Taboton DG. 1997. A new method for the determination of flow directions and contributing areas in grid digital elevation models. Water Resources Research 33(2):309-319.
+
+Tesfa TK, Tarboton DG, Watson DW, Schreuders KAT, Baker ME, Wallace RM. 2011. Extracting hydrological proximity measures from DEMs using parallel processing. Environmental Modelling and Software 26(12): 1696-1706. http://dx.doi.org/10.1016/j.envsoft.2011.07.018
---
+++ flow.py
@@ -0,0 +1,132 @@
+#! /usr/bin/env python
+
+"""
+C:\Program Files\TauDEM\TauDEM5Exe
+"""
+import argparse, subprocess, os, sys, time, glob, shlex, re
+
+def getArgs():
+	parser = argparse.ArgumentParser(
+		description = """PyFlow is a pipeline using TauDEM to calculate Flow Accumulation for multiscale elevation output of Surface Analysis Tools"""
+	)
+	parser.add_argument(
+		"-i",
+		"--input",
+		type = str,
+		required = True,
+		help = "Input folder containing 1 or more DEM in GeoTiff (.tif) format"
+	)
+	parser.add_argument(
+		"-o",
+		"--output",
+		type = str,
+		required = True,
+		help = "Empty output directory where results of TauDEM will be saved"
+	)
+	parser.add_argument(
+		"-e",
+		"--executables",
+		type = str,
+		required = True,
+		help = "Path to TauDEM Executables Folder"
+	)
+	parser.add_argument(
+		"-m",
+		"--method",
+		type = int,
+		required = True,
+		choices=[0,1],
+		help = "Either D-8 (0) or D-Infinity (1) Algorithm. Default is D-Infinity."
+
+	)
+	parser.add_argument(
+		"-v",
+		"--verbose",
+		action = "store_true",
+		help = "Print status updates while executing"
+	)
+	return parser.parse_args()
+
+def commandline(cmd, verbose=False):
+	if verbose:
+		print(cmd)
+	#cmd_args = shlex.split(cmd)
+	stdout, stderr = sp.Popen(
+		cmd_args,
+		stdin = sp.PIPE,
+		stdout = sp.PIPE,
+		stderr = sp.PIPE
+	).communicate()
+	if self.args.verbose:
+		print stdout, stderr
+	return True
+
+def rmPits(cmd_dict):
+	cmd = "{exec}\\PitRemove.exe -z {input}\\{file} -fel {output}\\rmpit\\{file}".format(
+		**cmd_dict
+	)
+	commandline(cmd)
+	return True	
+
+def flowdir(cmd_dict):
+	if cmd_dict['method']==0:
+		cmd = "{exec}\\D8FlowDir.exe -fel {output}\\rmpit\\{file} -sd8 \
+{output}\\slope\\slope{scale}.tif -p {}\\fd\\fd{}.tif".format(**cmd_dict)
+	else:
+		cmd = "{exec}\\DinfFlowDir.exe -fel {output}\\rmpit\\{file} -slp\
+ {output}\\slope\\slope{scale}.tif -ang {}\\fd\\fd{}.tif".format(**cmd_dict)
+ 	commandline(cmd)
+ 	return True
+
+def area(cmd_dict):
+	if cmd_dict['method']==0:
+		cmd = "{exec}\\AreaD8.exe -p {output}\\fd\\fd{scale}.tif -ad8 \
+{output}\\fa\\fa{scale}.tif".format(**cmd_dict)
+	else:
+		cmd = "{exec}\\AreaDinf.exe -ang {output}\\fd\\fd{scale}.tif\
+ -sca {output}\\fa\\fa{scale}.tif".format(**cmd_dict)
+ 	commandline(cmd)
+ 	return True
+
+def driver(args):
+	base = os.getcwd()
+	os.chdir(args.input)
+	files = glob.glob("*")
+	os.chdir(base)
+	# Make directories for output files
+	os.chdir(args.output)
+	# intermediate files
+	os.mkdir("rmpit")
+	os.mkdir("fd")
+	os.mkdir("slope")
+	# Desired files
+	os.mkdir("fa")
+	os.chdir(base)
+	# run for each file at each scale
+	for f in files:
+		if args.verbose:
+			print("Processing file {}...".format(f))
+		scale = re.findall(r'\d+', f)[0]
+		cmd_dict = {
+			'exec':args.executables,
+			'input':args.input,
+			'output':args.output,
+			'file':f,
+			'scale':scale,
+			'method':args.method
+		}
+		rmPits(cmd_dict)
+		flowdir(cmd_dict)
+		area(cmd_dict)
+	return True
+
+def main():
+	t_i = time.time()
+	args = getArgs()
+	driver(args)
+ 	t_f = time.time()
+ 	if args.verbose:
+ 		print("Total elapsed time was {} s".format((t_f-t_i)/60.))
+
+if __name__ == "__main__":
+	main()
